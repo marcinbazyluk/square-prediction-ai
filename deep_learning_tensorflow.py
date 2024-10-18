1e8bfde12ab98492b36fac7bb0af7de2593246bc
Marcin Bazyluk <bazyluk@gmail.com>
2024-10-18 11:32:07 +0200

    Developing the tensorflow script

---
+++ deep_learning_tensorflow.py
@@ -0,0 +1,126 @@
+import random
+import numpy
+import tensorflow
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+data_set_size = 50
+x_range = 2000
+y_range = 2000
+
+x_list = []
+y_list = []
+outcome = []
+# let's assume that all pairs of numbers for which each number in a pair is within the 1-1000 range should return True
+for i in range(0, int(data_set_size/4)):
+  x = random.randint(0, int(x_range/2))
+  x_list.append(x)
+  y = random.randint(0, int(y_range/2))
+  y_list.append(y)
+  outcome.append(True)
+
+# now let's assume that all other pairs of numbers should return False
+for i in range(0, int(data_set_size/4)):
+  x = random.randint(0, int(x_range/2))
+  x_list.append(x)
+  y = random.randint(int(y_range/2), y_range)
+  y_list.append(y)
+  outcome.append(False)
+
+for i in range(0, int(data_set_size/4)):
+  x = random.randint(int(x_range/2), x_range)
+  x_list.append(x)
+  y = random.randint(0, int(y_range/2))
+  y_list.append(y)
+  outcome.append(False)
+
+for i in range(0, int(data_set_size/4)):
+  x = random.randint(int(x_range/2), x_range)
+  x_list.append(x)
+  y = random.randint(int(y_range/2), y_range)
+  y_list.append(y)
+  outcome.append(False)
+
+# merging x_list, y_list and outcome into a 2-dim array which will be our dataset
+data = numpy.vstack((x_list, y_list)).T
+
+X_train, X_test, y_train, y_test = train_test_split(data, outcome, test_size=0.2)
+print("Number of samples in training set: " + str(X_train.shape[0]))
+print("Number of samples in test set: " + str(X_test.shape[0]))
+
+# Encode the data as PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float)
+X_test = torch.tensor(X_test, dtype=torch.float)
+y_train = torch.tensor(y_train, dtype=torch.long)
+y_test = torch.tensor(y_test, dtype=torch.long)
+
+# define the model
+tensorflow.random.set_seed(10) # seed for reproducible result
+tensorflow.keras.utils.set_random_seed(10)
+
+model = tensorflow.keras.Sequential([
+    tensorflow.keras.layers.Dense(20, activation='relu', input_shape=(8,)),
+    tensorflow.keras.layers.Dense(10, activation='relu'),
+    tensorflow.keras.layers.Dense(2, activation='softmax')
+])
+
+
+# compile the model and define the loss function, the optimizer, and the training epochs.
+
+model.compile(
+    optimizer=tensorflow.keras.optimizers.Adam(learning_rate=0.01),
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+epochs = 200
+
+# train the model
+model.fit(X_train, y_train, epochs=epochs, verbose=0.5)
+
+print("Model training complete")
+
+# test the model
+y_predicted_probabilities = model.predict(X_test)
+y_predicted = tensorflow.argmax(y_predicted_probabilities, axis=1)
+
+# Compare the predicted values for the test set (y_predicted) against the expected values (y_test)
+print("Classification Report:")
+print(classification_report(y_test, y_predicted))
+
+# test the model with sample cases
+
+X_samples = []
+X_samples.append([500, 500])	# True
+X_samples.append([450, 120])	# True
+X_samples.append([0, 200])	# True
+X_samples.append([50, 900])	# True
+X_samples.append([999, 999])	# True
+X_samples.append([1500, 700])	# False
+X_samples.append([700, 1500])	# False
+X_samples.append([1500, 1500])	# False
+X_samples.append([1001, 1001])	# False
+X_samples.append([2000, 2000])	# False
+
+outputs = (False, True)
+def predict(samples):
+    inputs_dataframe = pandas.DataFrame(samples)
+    inputs_tensor = torch.FloatTensor(inputs_dataframe.values)
+    predictions = []
+    for case in inputs_tensor:
+        predictions_tensor = model(case)
+        prediction_index = predictions_tensor.argmax().item()
+        predictions.append(outputs[prediction_index])
+    return predictions
+    
+#def predict(samples):
+#    features_as_lists = [list(sample.values()) for sample in samples]
+#    inputs_array = np.array(features_as_lists)
+#    prediction_probabilities = model.predict(inputs_array, verbose=0)
+#    # argmax gets the index of the maximum value in an array
+#    predictions = [classes[np.argmax(p)] for p in prediction_probabilities]
+#    return predictions
+
+#predictions = predict(X_samples)
+#print(predictions)
